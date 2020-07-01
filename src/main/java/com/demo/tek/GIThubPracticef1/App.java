78b6edef525c0da1287c7904abd43e1b791107e4
Rohullah <rohullah.teksoa@gmail.com>
2020-07-01 10:39:52 -0400

    First Practice

---
+++ src/main/java/com/demo/tek/GIThubPracticef1/App.java
@@ -0,0 +1,13 @@
+package com.demo.tek.GIThubPracticef1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
